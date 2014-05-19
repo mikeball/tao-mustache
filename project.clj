fb5177644d500264568cdb274b9d80cd0e02547c
Mike Ball <michaelhball@gmail.com>
2014-05-18 22:50:05 -0700

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject org.taoclj/tao-mustache "0.0.2-SNAPSHOT"
+(defproject org.taoclj/tao-mustache "0.0.2"
+
   :description "Mustache templates with inheritance."
   :url "https://github.com/mikeball/tao-mustache"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.7.3"]
+                 [org.clojure/tools.reader "0.8.3"]
                  [me.shenfeng/mustache "1.1"]
-                 [pathetic "0.5.1"]])
+                 [pathetic "0.5.1"]
+                 ])
